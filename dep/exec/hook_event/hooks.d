96bf6f915c1ea837b36a7db5a0a8456707cdd347
Jean-Michel Dusserre <jedusser@z3r11p1.42lyon.fr>
2024-11-14 08:45:44 +0100

    new file struct

---
+++ dep/exec/hook_event/hooks.d
@@ -0,0 +1,18 @@
+obj/exec/hook_event/hooks.o: src/exec/hook_event/hooks.c \
+  src/exec/hook_event/../../../include/cub3D.h \
+  include/../minilibx-linux/mlx.h \
+  include/headers/get_next_line_h/get_next_line.h \
+  include/headers/cub3d_structs.h include/headers/cub3d_define.h \
+  include/headers/cub3d_functions.h
+
+src/exec/hook_event/../../../include/cub3D.h:
+
+include/../minilibx-linux/mlx.h:
+
+include/headers/get_next_line_h/get_next_line.h:
+
+include/headers/cub3d_structs.h:
+
+include/headers/cub3d_define.h:
+
+include/headers/cub3d_functions.h:
