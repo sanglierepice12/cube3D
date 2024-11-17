ffab28c960c1cee014d5078a77f03fb0b74089ce
¨michmuch¨ <jean-michel.dusserre@hotmail.fr>
2024-11-17 11:22:34 +0100

    update : use o macros for redudndant calculation --> better clarity, use of libft functions, implemeted floor and ceiling fullfilling, and bit of optimization

--- src/exec/render_3d_map.c
+++ src/exec/render_3d_map.c
@@ -6,7 +6,7 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:39:11 by jedusser          #+#    #+#             */
-/*   Updated: 2024/11/17 11:15:44 by jedusser         ###   ########.fr       */
+/*   Updated: 2024/11/17 11:21:58 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,20 +44,20 @@ void draw_3d_column(t_game *game)
 }
 void ray_caster(t_game *game, t_player *player, t_raycaster *raycaster, t_proj *projection)
 {
+    float step_x;
+    float step_y;
+    
     projection->distance_to_wall = DISTANCE(raycaster->ray_x, raycaster->ray_y, player->player_px_pos_x, player->player_px_pos_y);
     projection->wall_height = (int)(TILE_SIZE * DISTANCE_TO_PLANE / projection->distance_to_wall);
     projection->wall_start = SCREEN_CENTER_Y - (projection->wall_height / 2);
     projection->wall_end = SCREEN_CENTER_Y + (projection->wall_height / 2);
-
-    float step_x = cos(raycaster->ray_angle);
-    float step_y = -sin(raycaster->ray_angle);
-
+    step_x = cos(raycaster->ray_angle);
+    step_y = -sin(raycaster->ray_angle);
     while (game->map->map[(int)(raycaster->ray_y / TILE_SIZE)][(int)(raycaster->ray_x / TILE_SIZE)] != '1')
     {
         raycaster->ray_x += step_x;
         raycaster->ray_y += step_y;
     }
-
     draw_3d_column(game);
 }
 
@@ -65,26 +65,23 @@ void ray_caster(t_game *game, t_player *player, t_raycaster *raycaster, t_proj *
 
 void render_3d_map(t_game *game, t_player *player, t_raycaster *raycaster, t_proj *projection)
 {
+    float ray_cos;
+    float ray_sin;
+    
     clear_image(game->game_img, BLACK, LIGHT_BLUE);
     raycaster->ray_index = 0;
-
     while (raycaster->ray_index < GAME_WIDTH)
     {
         raycaster->ray_angle = player->angle - FOV_HALF + (raycaster->ray_index * RAY_ANGLE_DELTA);
-
-        // Precompute sin and cos
-        float ray_cos = cos(raycaster->ray_angle);
-        float ray_sin = sin(raycaster->ray_angle);
-
+        ray_cos = cos(raycaster->ray_angle);
+        ray_sin = sin(raycaster->ray_angle);
         raycaster->ray_x = player->player_px_pos_x;
         raycaster->ray_y = player->player_px_pos_y;
-
         while (game->map->map[(int)(raycaster->ray_y / TILE_SIZE)][(int)(raycaster->ray_x / TILE_SIZE)] != '1')
         {
             raycaster->ray_x += ray_cos;
             raycaster->ray_y += ray_sin;
         }
-
         ray_caster(game, player, raycaster, projection);
         raycaster->ray_index++;
     }
