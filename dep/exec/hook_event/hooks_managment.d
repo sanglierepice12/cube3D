fde4ed892776724bb088524be83364903260740b
Jean-Michel Dusserre <jedusser@z1r5p2.42lyon.fr>
2024-11-27 11:09:33 +0100

    hello

---
+++ dep/exec/hook_event/hooks_managment.d
@@ -0,0 +1,25 @@
+obj/exec/hook_event/hooks_managment.o: \
+  src/exec/hook_event/hooks_managment.c include/../include/cub3D.h \
+  include/../minilibx-linux/mlx.h \
+  include/headers/get_next_line_h/get_next_line.h \
+  include/headers/define.h include/headers/struct.h \
+  include/headers/exec_functions.h include/headers/parsing_functions.h \
+  include/headers/utils_functions.h include/../minilibx-linux/mlx_int.h
+
+include/../include/cub3D.h:
+
+include/../minilibx-linux/mlx.h:
+
+include/headers/get_next_line_h/get_next_line.h:
+
+include/headers/define.h:
+
+include/headers/struct.h:
+
+include/headers/exec_functions.h:
+
+include/headers/parsing_functions.h:
+
+include/headers/utils_functions.h:
+
+include/../minilibx-linux/mlx_int.h:
---
+++ dep/parsing/get_next_line/get_next_line_utils.d
@@ -0,0 +1,24 @@
+obj/parsing/get_next_line/get_next_line_utils.o: \
+  src/parsing/get_next_line/get_next_line_utils.c \
+  src/parsing/get_next_line/../../../include/cub3D.h \
+  include/../minilibx-linux/mlx.h \
+  include/headers/get_next_line_h/get_next_line.h \
+  include/headers/define.h include/headers/struct.h \
+  include/headers/exec_functions.h include/headers/parsing_functions.h \
+  include/headers/utils_functions.h
+
+src/parsing/get_next_line/../../../include/cub3D.h:
+
+include/../minilibx-linux/mlx.h:
+
+include/headers/get_next_line_h/get_next_line.h:
+
+include/headers/define.h:
+
+include/headers/struct.h:
+
+include/headers/exec_functions.h:
+
+include/headers/parsing_functions.h:
+
+include/headers/utils_functions.h:
