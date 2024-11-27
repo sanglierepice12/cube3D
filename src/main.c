0f853a87ba83609cbaf2564083b8fe212ee68bbd
Jean-Michel Dusserre <jedusser@z1r5p2.42lyon.fr>
2024-11-27 15:39:47 +0100

    hello

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 05:22:20 by jedusser          #+#    #+#             */
-/*   Updated: 2024/11/22 15:44:35 by sanglier         ###   ########.fr       */
+/*   Updated: 2024/11/27 15:39:29 by jedusser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int main(int arc, char **argv)
 	init_parse(game, argv[1]);
 	init_game(game);
 	hook_management(game);
-	mlx_loop_hook(game->mlx_data.mlx_ptr, &draw_and_display_map, &game);
+	mlx_loop_hook(game->mlx_data.mlx_ptr, &draw_and_display_map, game);
 	draw_and_display_map(game);
 	mlx_loop(game->mlx_data.mlx_ptr);
 	exit_prog(game);
