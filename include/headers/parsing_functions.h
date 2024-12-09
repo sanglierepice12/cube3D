54715a4824c73cc3c8bb032e7778393017ee587e
Gonzague Suter <sanglier@MacBook-Pro-de-Gonzague.local>
2024-12-09 15:51:55 +0100

    merge 9/12

---
+++ include/headers/parsing_functions.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_functions.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsuter <gsuter@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/13 12:57:20 by gsuter            #+#    #+#             */
+/*   Updated: 2024/11/13 12:57:20 by gsuter           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_FUNCTIONS_H
+# define PARSING_FUNCTIONS_H
+
+/****INIT****/
+void	init_struct(t_game **game);
+void	init_parse(t_game *game, char *file);
+
+
+/*******CHECK_TEXTURES******/
+bool	check_texture(char	*line);
+bool	check_rgb(char *line);
+
+/****IS_LINE_SOMETHING***/
+bool	is_line_full_spaces(char *line);
+bool	is_line_ok(char	*temp);
+bool	is_line_m_ok(char *line);
+void	wall_is_good(t_game *game, char *line, bool flag);
+
+/*****GET_MAP_TEXTURE_RGB*****/
+void	first_line(int fd, t_list **list, t_game *game);
+void	fill_map_to_list(t_game *game, t_list **list, int fd);
+void	get_rgb(char *line, t_game *game);
+void	get_textures(char *line, t_texture *texture, t_game *game);
+void	fill_tex(char *line, t_texture *texture, e_txt type);
+void	fill_rgb(char *line, t_game *game, e_rgb type);
+void	fill_list_to_map(t_game *game, t_list **list);
+void	fill_playerpos(char *line, t_game *game, size_t len);
+
+/****MAP_VALIDITY****/
+void	eye_tomap(char **map, size_t x, size_t y, t_game *game);
+
+#endif
---
+++ src/parsing/init_the_map/get_map.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_map.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 12:42:26 by jedusser          #+#    #+#             */
+/*   Updated: 2024/11/13 13:03:35 by gsuter           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/cub3D.h"
+
+void	first_line(int fd, t_list **list, t_game *game)
+{
+	char	*line;
+
+	line = NULL;
+	while (777)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			simple_exit("Nothing in the file ...", 1);
+		get_textures(line, game->map->texture, game);
+		get_rgb(line, game);
+		if ((line + parse_ws(line))[0] == '1' && game->map->count == 6)
+			break ;
+		if (!is_line_ok(line))
+			exit_prog(game);
+		free(line);
+	}
+	*list = ft_new_node(line);
+	free(line);
+	line = NULL;
+}
+
+static int	open_map(char *file, t_game *game)
+{
+	int	fd;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error occurred while opening the document...");
+		exit_prog(game);
+	}
+	return (fd);
+}
+
+static bool	check_ext(char *file)
+{
+	size_t	len;
+
+	len = ft_strlen(file);
+	if (len < 4)
+		return (printf ("Error: Wrong extension."), false);
+	if (ft_strncmp(file + (len - 4), ".cub", 4))
+		return (printf ("Error: Wrong extension, it's not a .cub."), false);
+	return (true);
+}
+
+
+void	init_parse(t_game *game, char *file)
+{
+	int	fd;
+
+	if (!check_ext(file))
+		exit_prog(game);
+	fd = open_map(file, game);
+	fill_map_to_list(game, &game->list, fd);
+	close(fd);
+	fill_list_to_map(game, &game->list);
+	free_list(game->list);
+	if (game->map->count != 7)
+	{
+		printf("Error, no player position\n");
+		exit_prog(game);
+	}
+	eye_tomap(game->map->map, game->player.player_pos_x, game->player.player_pos_y, game);
+}
